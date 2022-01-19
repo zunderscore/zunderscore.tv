7af4d642f9f9bc330a2d37cfda8ee503ed8b7014
Zack Williamson <zwilliamson@outlook.com>
2022-01-18 22:29:14 -0500

    Here goes v2

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+using System.Threading.Tasks;
+
+namespace ZunderscoreTv
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args) =>
+            await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .AddSetting(WebKeys.MakeLinksRootRelative, true)
+                .AddSetting(Keys.LinksUseHttps, true)
+                .RunAsync();
+    }
+}
